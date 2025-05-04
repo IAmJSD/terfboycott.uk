d9ca0a19f16cc7283c3ab3d24ff5386fbb7b7458
Astrid Gealer <astrid@gealer.email>
2025-05-04 20:23:21 +0100

    add more things

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -22,3 +22,39 @@ export const labour: Data = {
 	referenceUrl:
 		"https://www.thepinknews.com/2021/09/24/labour-party-transphobia-keir-starmer-rosie-duffield/",
 };
+
+export const conservatives: Data = {
+	name: "Conservatives",
+	iconUrl: "/brands/conservatives.png",
+	description:
+		"As a political party, the Conservatives have constantly defended and partied about transphobia.",
+	referenceUrl:
+		"https://www.thepinknews.com/2023/10/06/rishi-sunak-transphobic-comments-conservative-conference/",
+};
+
+export const sexMatters: Data = {
+	name: "Sex Matters",
+	iconUrl: "/brands/sex-matters.png",
+	description:
+		"Sex Matters is a transphobic and TERF organisation that was responsible for the Supreme Court case that led to the transphobic judgement and guidance.",
+	referenceUrl:
+		"https://www.supremecourt.uk/cases/judgments/uksc-2024-0042",
+};
+
+export const ehrc: Data = {
+	name: "Equality and Human Rights Commission",
+	iconUrl: "/brands/ehrc.png",
+	description:
+		"The Equality and Human Rights Commission has issued highly transphobic, exclusionary guidance on the definition of sex. You do not need to follow it legally, they are not law enforcement.",
+	referenceUrl:
+		"https://transactual.org.uk/blog/2025/04/26/statement-on-ehrc-guidance-on-single-sex-spaces/",
+};
+
+export const btp: Data = {
+	name: "British Transport Police",
+	iconUrl: "/brands/btp.png",
+    description:
+        "Since the judgement, the British Transport Police were very quick to jump upon it and use it to make sure trans women get strip searched by men.",
+	referenceUrl:
+        "https://news.sky.com/story/trans-women-to-be-strip-searched-by-male-transport-police-after-court-ruling-13350577",
+};
